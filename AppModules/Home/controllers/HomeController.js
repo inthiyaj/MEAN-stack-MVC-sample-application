31afc1a41142852700c225e1aae16a016f47ded8
George sedky <georgesedky@kajo.speedport_w_723v_1_39_000>
2016-03-12 22:53:42 +0100

    UPDATE HomeController to utilize view_lib module

--- AppModules/Home/controllers/HomeController.js
+++ AppModules/Home/controllers/HomeController.js
@@ -2,13 +2,12 @@ module.exports = function(app, route, express) {
 
 /////////////////////////////////////////////////////////////////
 
-  var path = __dirname.split('/');
-  path.pop();
-  path = path.join('/') + "/views/";
+  var view_lib = require('_controller_libs/view_lib');
+  var view_path = view_lib.get_my_view_path(__dirname);
 
   app.get('/', function(req, res){
-    app.set('views', path);
-    app.use("/Home/myApp", express.static(path + "myApp.js"));
+    view_lib.set_my_view_route(app, view_path);
+    view_lib.add_res_file(express, app, view_path, "myApp.js");
     res.render('index.html');
   });
 
